18970ed2e7522157c607fd9ac6c0d10812781866
hsmgowtham <hsmgowtham@gmail.com>
2024-05-19 10:29:20 -0400

    Updates

---
+++ precache-manifest.fa4012a3b437a9e04bd43f900ee2ac70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c124d2aa0c256686156e1dd990eea2",
+    "url": "/React-JS/index.html"
+  },
+  {
+    "revision": "563d8bb985485971e0c9",
+    "url": "/React-JS/static/css/main.57e8f2c5.chunk.css"
+  },
+  {
+    "revision": "3a1e766a403a9748e80a",
+    "url": "/React-JS/static/js/2.958092a7.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/React-JS/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "563d8bb985485971e0c9",
+    "url": "/React-JS/static/js/main.e6b1db29.chunk.js"
+  },
+  {
+    "revision": "668da7370613c6977cab",
+    "url": "/React-JS/static/js/runtime~main.bef05391.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React-JS/static/media/logo.06e73328.svg"
+  }
+]);
