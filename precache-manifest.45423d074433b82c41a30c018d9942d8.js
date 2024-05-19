290f75aeb06820e5a6c29c5985b2113b02a3e0bd
hsmgowtham <hsmgowtham@gmail.com>
2024-05-19 10:15:27 -0400

    Updates

---
+++ precache-manifest.45423d074433b82c41a30c018d9942d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7772ce3ce691a7bc412a1bb65afdbe1",
+    "url": "/eact-app-practice/index.html"
+  },
+  {
+    "revision": "563d8bb985485971e0c9",
+    "url": "/eact-app-practice/static/css/main.57e8f2c5.chunk.css"
+  },
+  {
+    "revision": "3a1e766a403a9748e80a",
+    "url": "/eact-app-practice/static/js/2.958092a7.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/eact-app-practice/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "563d8bb985485971e0c9",
+    "url": "/eact-app-practice/static/js/main.e6b1db29.chunk.js"
+  },
+  {
+    "revision": "2c2f174df37aaacf1fb5",
+    "url": "/eact-app-practice/static/js/runtime~main.4f6ae6c8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/eact-app-practice/static/media/logo.06e73328.svg"
+  }
+]);
