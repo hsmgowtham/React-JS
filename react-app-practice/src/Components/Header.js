4518ab22104780c14204af2690be04d4063df68f
hsmgowtham <hsmgowtham@gmail.com>
2024-05-19 10:08:25 -0400

    Update Github pages

--- react-app-practice/src/Components/Header.js
+++ react-app-practice/src/Components/Header.js
@@ -4,7 +4,7 @@ import '../App.css';
 export default function Navbar() {
   return (
       <nav>
-          <img src={logo} className="nav--icon" />
+          <img src={logo} className="nav--icon" alt='nav-icon' />
           <h3 className="nav--logo_text">ReactFacts</h3>
           <h4 className="nav--title">React JS - Project 1</h4>
       </nav>
